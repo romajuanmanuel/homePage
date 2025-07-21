e2c85b5ff6596d7b4e6bf6d0d5b8941dd6b9e52e
Romaxolote <romajuanmanuel@gmail.com>
2025-07-21 15:05:56 -0300

    base files

---
+++ src/index.js
@@ -0,0 +1,4 @@
+  import "./styles.css";
+
+    const app = document.getElementById("app");
+    app.innerHTML = "<p>This content was injected by JavaScript.</p>";
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+    const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+    module.exports = {
+      mode: "development",
+      entry: "./src/index.js",
+      output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+      },
+      devtool: "eval-source-map",
+      devServer: {
+        static: "./dist",
+        watchFiles: ["./index.html"],
+        hot: true,
+        open: true
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: "./index.html",
+        }),
+      ],
+      module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+          {
+            test: /\.html$/i,
+            loader: "html-loader",
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+            type: "asset/resource",
+          },
+        ],
+      },
+    };
