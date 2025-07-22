9b442f6df31504ef3d281741b3ecf48671ff551d
Romaxolote <romajuanmanuel@gmail.com>
2025-07-22 12:44:44 -0300

    initial elements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
   import "./styles.css";
 
-    const app = document.getElementById("app");
-    app.innerHTML = "<p>This content was injected by JavaScript.</p>";
+   const app = document.getElementById("app");
+
+// Limpia el contenido inicial (si es necesario)
+app.textContent = ""; 
+
+const header = document.createElement("header");
+header.id = "header";
+header.textContent = "Header content";
+app.appendChild(header);
+
+const main = document.createElement("main");
+main.id = "main";
+main.textContent = "Main content goes here.";
+app.appendChild(main);
+
+const footer = document.createElement("footer");
+footer.id = "footer";
+footer.textContent = "Footer content";
+app.appendChild(footer);
+
+    
