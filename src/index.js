45a54be96a9e686a990d8eed38f0c87527512302
Romaxolote <romajuanmanuel@gmail.com>
2025-07-23 10:06:31 -0300

    add card

--- src/index.js
+++ src/index.js
@@ -3,29 +3,33 @@ import headerPhoto from "./assets/headerPhoto.jpg";
 
 const app = document.getElementById("app");
 app.innerHTML = "";
-
-
 const header = document.createElement("header");
 header.id = "header";
-
-const title = document.createElement("h1");
-title.textContent = "Header content";
-header.appendChild(title);
-
 const img = document.createElement("img");
 img.src = headerPhoto;
 img.alt = "Header Photo";
 header.appendChild(img);
 
-app.appendChild(header);
+const card = document.createElement("div");
+const title = document.createElement("h1");
+title.textContent = "About Me";
+
+const text = document.createElement("p");
+text.textContent = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+card.className = "card";
+card.appendChild(title);
+card.appendChild(text);
+header.appendChild(card);
+
+
 
 const main = document.createElement("main");
 main.id = "main";
 main.textContent = "Main content goes here.";
-app.appendChild(main);
 
 const footer = document.createElement("footer");
 footer.id = "footer";
 footer.textContent = "Footer content";
-app.appendChild(footer);
 
+app.append(header,main,footer);
